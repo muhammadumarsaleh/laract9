60c2f0b238cd49d699eb9e0d9fa520915478ae7c
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2022-07-15 18:08:48 +0800

    membuat project portal berita menggunakan laravel dan react, setting invironment for laravel dan react, make model dan migration

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\News;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        News::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Homepage(props){
+    return (
+        <div>
+            <h1>HOMEPAGE</h1>
+        </div>
+    )
+}
